413a0b7a84847ed8a58c069fb33bcdc3519c632a
alexbejan-vgw <92439949+alexbejan-vgw@users.noreply.github.com>
2023-05-30 00:56:42 +0300

    Update reducer

--- lib/state/AppReducer.dart
+++ lib/state/AppReducer.dart
@@ -15,68 +15,78 @@ import 'actions/toggle-provider-favaourite.dart';
 AppState reducer(AppState state, dynamic action) {
   AppState newState = state;
 
-  if (action is FetchTimeAction) {
-    newState.location = action.location;
-    newState.time = action.time;
+  switch (action) {
+    case FetchTimeAction: {
+      newState.location = action.location;
+      newState.time = action.time;
 
-    return newState;
-  } else if (action is ChangePageAction) {
-    newState.page_type = action.page_type;
-
-    return newState;
-  } else if (action is GetDataAction) {
-    newState.providers = action.providers;
-
-    return newState;
-  } else if (action is OpenProviderPageAction) {
-    newState.selectedProvider = action.selectedProvider;
-    newState.page_type = PageTypes.providerPage;
-
-    return newState;
-  } else if (action is OpenProductPageAction) {
-    newState.selectedProduct = action.selectedProduct;
-    newState.selectedProvider = action.selectedProvider;
-    newState.page_type = PageTypes.detailPage;
-
-    return newState;
-  } else if (action is ToggleFavouriteStatus) {
-    ProviderModel provider = action.provider;
-    provider.isFavourite = action.isFavourite;
-
-    if (action.isFavourite) {
-      newState.favouriteProviders[provider.id] = provider;
-    } else {
-      newState.favouriteProviders.remove(provider.id);
+      return newState;
     }
+    case ChangePageAction: {
+      newState.page_type = action.page_type;
 
-    return newState;
-  } else if (action is ChangeCurrentIndex) {
-    newState.navigationBarIndex = action.nav_bar_index;
-
-    return newState;
-  } else if (action is ChangeUserPage) {
-    newState.userPage = action.page_state;
-
-    return newState;
-  } else if (action is AddProductToCart) {
-    var quantity = newState.selectedQuantities[action.product.id] ?? 0;
-    if (quantity == 0) {
-      newState.cartProducts.add(action.product);
+      return newState;
     }
+    case GetDataAction: {
+      newState.providers = action.providers;
 
-    newState.selectedQuantities[action.product.id] = quantity + 1;
-
-    return newState;
-  } else if (action is UpdateProductQuantity) {
-    if (action.quantity <= 0) {
-      newState.selectedQuantities.remove(action.productId);
-      newState.cartProducts.removeWhere((p) => p.id == action.productId);
-    } else {
-      newState.selectedQuantities[action.productId] = action.quantity;
+      return newState;
     }
+    case OpenProviderPageAction: {
+      newState.selectedProvider = action.selectedProvider;
+      newState.page_type = PageTypes.providerPage;
 
-    return newState;
-  } else {
-    return state;
+      return newState;
+    }
+    case OpenProductPageAction: {
+      newState.selectedProduct = action.selectedProduct;
+      newState.selectedProvider = action.selectedProvider;
+      newState.page_type = PageTypes.detailPage;
+
+      return newState;
+    }
+    case ToggleFavouriteStatus: {
+      ProviderModel provider = action.provider;
+      provider.isFavourite = action.isFavourite;
+
+      if (action.isFavourite) {
+        newState.favouriteProviders[provider.id] = provider;
+      } else {
+        newState.favouriteProviders.remove(provider.id);
+      }
+
+      return newState;
+    }
+    case ChangeCurrentIndex: {
+      newState.navigationBarIndex = action.nav_bar_index;
+
+      return newState;
+    }
+    case ChangeUserPage: {
+      newState.userPage = action.page_state;
+
+      return newState;
+    }
+    case AddProductToCart: {
+      var quantity = newState.selectedQuantities[action.product.id] ?? 0;
+      if (quantity == 0) {
+        newState.cartProducts.add(action.product);
+      }
+
+      newState.selectedQuantities[action.product.id] = quantity + 1;
+
+      return newState;
+    }
+    case UpdateProductQuantity: {
+      if (action.quantity <= 0) {
+        newState.selectedQuantities.remove(action.productId);
+        newState.cartProducts.removeWhere((p) => p.id == action.productId);
+      } else {
+        newState.selectedQuantities[action.productId] = action.quantity;
+      }
+
+      return newState;
+    }
+    default: return state;
   }
 }
